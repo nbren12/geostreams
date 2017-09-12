9ad60c1553edfa442863aa42e8ccf2c9bfd95251
Noah D. Brenowitz <nbren12@gmail.com>
2017-09-12 16:05:17 -0700

    Add simple fortran program which passes data to redis

---
+++ game_of_life/main.f90
@@ -0,0 +1,9 @@
+program name
+  use redis_mod
+  implicit none
+  integer, parameter ::n=100, m=100
+  integer f(n,m)
+
+  call stream_data(f)
+
+end program name
---
+++ game_of_life/redis_wrapper.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hiredis.h>
+// did brew install hiredis
+
+
+void publish_to_redis_(int* f, int* n , int* m){
+  printf("Hello World'");
+}
