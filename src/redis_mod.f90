cbe9d742d0aece6b16a508ba8860146acbb5e505
Noah D. Brenowitz <nbren12@gmail.com>
2017-09-14 21:24:09 +0000

    Fix compilation error on google cloud
    had to revert to an older version of the game_of_life

--- src/redis_mod.f90
+++ src/redis_mod.f90
@@ -24,5 +24,18 @@ module redis_mod
 
   end
 
+  subroutine stream_data(f)
+    use iso_c_binding
+    integer(c_int) :: f(:,:)
+    integer(c_int) :: n, m
+    external publish_to_redis
+    ! TODO this is a function stub
+    n = size(f, 1)
+    m = size(f, 2)
+
+    call publish_to_redis(f, n, m)
+
+  end subroutine stream_data
+
 
 end module redis_mod
