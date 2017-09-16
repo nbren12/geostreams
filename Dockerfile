a5462c72cbb3fab629e0e5212c627a3823aff64b
Noah D. Brenowitz <nbren12@gmail.com>
2017-09-15 23:50:17 -0700

    Use docker-compose for managing containers
    - add a docker-compose.yml file which basically contains all the
    information which wasin the ./run_docker_cluster.sh script.
    
    - use bind mounts instead of manual add statements in the dockerfile.
    I think ADD commands only run when the dockerfile is built,
    which is not ideal for development purposes.

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@ FROM debian:stretch
 
 RUN apt-get update && apt-get -y install gfortran mpich gcc make libhiredis-dev libhiredis0.13
 
-RUN mkdir /gameoflife
-ADD Makefile /gameoflife/Makefile
-ADD game_of_life /gameoflife/game_of_life
-ADD src /gameoflife/src
-ADD test /gameoflife/test
+#RUN mkdir /gameoflife
+#ADD Makefile /gameoflife/Makefile
+#ADD game_of_life /gameoflife/game_of_life
+#ADD src /gameoflife/src
+#ADD test /gameoflife/test
 
-RUN cd /gameoflife && make
-
-CMD cd /gameoflife/game_of_life && make && /bin/bash -c "echo 1 | ./game_of_life"
+CMD cd /gameoflife/ && make && \
+    cd /gameoflife/game_of_life && make && \
+    /bin/bash -c "echo 3 | ./game_of_life"
 
 
--- run_docker_cluster.sh
+++ run_docker_cluster.sh
@@ -1,32 +1,4 @@
 #!/bin/bash -e
-#
-# Build and run a docker container cluster with three containers, each running a service:
-#  * Game of Life (fortran implementation of Conway's game of life that pushes scenes to redis)
-#  * Redis server (for communicating scenes from game of life to clients)
-#  * Bokeh server (for displaying messages sent through redis)
-#
-# To run, just call this script.
-#
-# NOTE: This script will block on the bokeh server.  Control-C to shut it down.
-# This script will clean up running containers once the bokeh server quits.
-# 
-# NOTE: Container state is automatically removed when the containers stop.
+# run docker containers using docker-compose
 
-# build docker containers
-pushd scripts
-docker build . -t jdouglass/ghw2017_gameoflife_bokeh
-popd
-docker build . -t jdouglass/ghw2017_gameoflife
-
-REDIS_CONTAINER="`docker run --name=gameoflife-redis -d --rm redis`"
-
-# Get the IP address of the Redis server within our local docker cluster.
-REDIS_URL=`docker inspect $REDIS_CONTAINER | egrep '\"IPAddress\"' | egrep '([0-9]{1,3}\.){3}[0-9]{1,3}' -o | uniq`
-
-LIFE_CONTAINER=`docker run -d -eREDIS_PORT=6379 -eREDIS_URL=$REDIS_URL --link gameoflife-redis:redis -h gameoflife-redis -p 6379 --rm jdouglass/ghw2017_gameoflife`
-
-docker run -ti -p 8889:8889 -eREDIS_PORT=6379 -eREDIS_URL=$REDIS_URL --link gameoflife-redis:redis -h gameoflife-redis -p 6379 --rm jdouglass/ghw2017_gameoflife_bokeh
-
-# once the bokeh container stops, stop the redis and life containers.
-echo "Stopping and cleaning up containers"
-docker stop $REDIS_CONTAINER $LIFE_CONTAINER
+docker-compose up
