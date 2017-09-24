8bbc4b9660fef7448287c9e9cc40d6c1b35edef7
Noah D. Brenowitz <nbren12@gmail.com>
2017-09-23 18:03:38 -0700

    Get test exes directory using pytest fixtures

--- test/test_send.py
+++ test/test_send.py
@@ -5,53 +5,56 @@ from subprocess import Popen
 
 import numpy.testing
 
-from geostreams import client, get_test_exes
+from geostreams import client
+import pytest
 
+@pytest.fixture
+def exe_dir(request):
+    rootdir = request.config.rootdir
+    return os.path.join(rootdir, "build", "test")
 
-def run_project_exe(exe_name):
+def run_project_exe(exe_name, exe_dir=None):
     """Run exe in ./test directory and raise an error if it doesn't run correctly
     """
-    exe_dir = get_test_exes()
     exe = os.path.join(exe_dir, exe_name)
     ret =  Popen([exe], env=os.environ).wait()
     if ret != 0:
         raise RuntimeError(f"{exe_name} returned with code {ret}.")
 
 
-class ArrayTest(unittest.TestCase):
-    def test_read_from_redis(self):
-        key = 'A:1'
+def test_read_from_redis(exe_dir):
+    key = 'A:1'
 
-        run_project_exe('test_send')
-        # 200 rows, 100 columns
-        array = client.read_from_redis(key)
-        x, _ = numpy.mgrid[1:201, 1:101]
-        numpy.testing.assert_array_equal(
-            x,
-            array)
+    run_project_exe('test_send', exe_dir)
+    # 200 rows, 100 columns
+    array = client.read_from_redis(key)
+    x, _ = numpy.mgrid[1:201, 1:101]
+    numpy.testing.assert_array_equal(
+        x,
+        array)
 
-    def test_write_to_redis(self):
-        key = str(uuid.uuid4())
+def test_write_to_redis():
+    key = str(uuid.uuid4())
 
-        try:
-            array = numpy.tile(numpy.arange(100).reshape((100, 1)), (1, 200))
-            client.write_to_redis(key, array)
-        finally:
-            with client.gol_connection() as connection:
-                connection.delete(key)
+    try:
+        array = numpy.tile(numpy.arange(100).reshape((100, 1)), (1, 200))
+        client.write_to_redis(key, array)
+    finally:
+        with client.gol_connection() as connection:
+            connection.delete(key)
 
-    def test_exes(self):
-        for exe in ['test_send',
-                    'test_connect',
-                    'test_redis_push']:
-            print(f"Running {exe}")
-            run_project_exe(exe)
+def test_exes(exe_dir):
+    for exe in ['test_send',
+                'test_connect',
+                'test_redis_push']:
+        print(f"Running {exe}")
+        run_project_exe(exe, exe_dir)
 
-    def test_redis_push(self):
-        run_project_exe('test_redis_push')
+def test_redis_push(exe_dir):
+    run_project_exe('test_redis_push', exe_dir)
 
-        array = client.read_from_redis("A")
-        x, _ = numpy.mgrid[1:201, 1:101] + .5
-        numpy.testing.assert_array_equal(
-            x,
-            array)
+    array = client.read_from_redis("A")
+    x, _ = numpy.mgrid[1:201, 1:101] + .5
+    numpy.testing.assert_array_equal(
+        x,
+        array)
